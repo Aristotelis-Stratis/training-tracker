bab2237a22193c6e6713b9b214281ee693baf7b3
Aristotelis-Stratis <stratis.dev@outlook.com>
2025-12-27 22:38:05 +0100

    Initial Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+type SetRow = {
+  set: number;
+  prev?: string;     // z.B. "65 x 15"
+  kg: number | null;
+  reps: number | null;
+  done: boolean;
+};
+
+type Exercise = {
+  name: string;
+  sets: SetRow[];
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'training-tracker';
+  exercise: Exercise = {
+    name: 'Barbell Curl',
+    sets: [
+      { set: 1, prev: '65 x 15', kg: 65, reps: 15, done: true },
+      { set: 2, prev: '65 x 15', kg: 65, reps: 15, done: false },
+    ],
+  };
+
+  addSet() {
+    const next = this.exercise.sets.length + 1;
+    this.exercise.sets.push({ set: next, prev: '', kg: null, reps: null, done: false });
+  }
+
+  toggleDone(i: number) {
+    this.exercise.sets[i].done = !this.exercise.sets[i].done;
+  }
 }
